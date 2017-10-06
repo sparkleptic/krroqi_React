7de4f1f212134b6e7a964cae92f420deb6117c5a
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2017-10-06 11:56:58 +0000

    connection.reducer.js edited online with Bitbucket

--- src/reducers/connection.reducer.js
+++ src/reducers/connection.reducer.js
@@ -1,9 +1,9 @@
 import * as types from './../constants/actionTypes';
 
-export default (state = false, action) => {
+export default (state = { isConnected: false }, action) => {
     switch (action.type) {
         case types.CHECK_CONNECTION:
-            return action.isConnected;
+            return { ...state, isConnected: action.isConnected };
         default:
             return state;
     }
